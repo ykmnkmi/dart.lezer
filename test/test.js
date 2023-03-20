3dfa0d5b0ea51a2dd39c3f9c085081d280eb13f2
Olzhas Suleimen <amajisenpai@gmail.com>
2023-03-20 18:51:36 +0600

    sync 0.0.3

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 import { parser } from '../dist/index.es.js'
 
-let result = parser.parse('true')
+let result = parser.parse("'hello ${ null }!'")
 let cursor = result.cursor()
 
 do {
   if (cursor.type.isSkipped) {
-    continue
+    console.log(`[${cursor.from}, ${cursor.to}]: SKIPPED`)
+  } else {
+    console.log(`[${cursor.from}, ${cursor.to}]: ${cursor.name}`)
   }
 
-  console.log(`[${cursor.from}, ${cursor.to}]: ${cursor.name}`)
 } while (cursor.next())
